2f603873dc19c6093a937f5c4b20d6a00147fddb
Jan Niklas Marzahl <janniklasmarzahl@gmail.com>
2022-05-14 13:51:29 +0200

    connection to sanity and homepage

---
+++ components/FooterBanner.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+
+import { urlFor } from "../lib/client";
+
+const FooterBanner = ({
+  banner: { discount, largeText1, largeText2, saleTime },
+}) => {
+  return (
+    <div className="footer-banner-container">
+      <div className="banner-desc">
+        <div className="left">
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default FooterBanner;
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,37 @@
 import React from "react";
 
-const Home = () => {
+import Product from "../components/Product";
+import HeroBanner from "../components/HeroBanner";
+import FooterBanner from "../components/FooterBanner";
+import { client } from "../lib/client";
+
+const Home = ({ products, banner }) => {
   return (
     <>
-      HeroBanner
+      <HeroBanner heroBanner={banner.length && banner[0]} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["product 1", "product 2"].map((product) => product)}
+        {products?.map((product) => (
+          <Product key={product._id} {...product} />
+        ))}
       </div>
-      Footer
+      <FooterBanner banner={banner && banner[0]} />
     </>
   );
+};
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const banner = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, banner },
+  };
 };
 
 export default Home;
